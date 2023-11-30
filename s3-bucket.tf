b4f719226039199a61f26ad1e588408a099c8aeb
Your Name <you@example.com>
2023-11-30 08:25:39 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "instruqt-my-s3-bucket"
-  bucket_prefix = "instrqut-my-s3-bucket"
+  bucket_prefix = "ljj"
   acl    = "private"
 
   control_object_ownership = true
